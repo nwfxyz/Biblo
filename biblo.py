506d371a2f43b2cb9662a752847d5559b4e557aa
nwfxyz <ngweifeng@u.nus.edu>
2017-07-19 12:39:31 +0800

    newspaper
    added newspaper import

--- biblo.py
+++ biblo.py
@@ -8,11 +8,12 @@ Created on Sun Jul 16 17:27:07 2017
 import os
 import bs4
 import calendar
-
+from newspaper import Article
 import requests
 import dateparser
+import logging
 
-os.chdir(r'C:\Users\userpv\Documents\GitHub\Biblo')
+#os.chdir(r'C:\Users\userpv\Documents\GitHub\Biblo')
 # Import pandas
 import pandas as pd
 
@@ -22,15 +23,20 @@ def findsoup(soup,string):
         print(a.get_text().strip())
     return(soup.find_all(text = string))
 
-def getdate(string):
+def getdate(string, parsed = False):
     try:
         date = dateparser.parse(string)
         date =  '(' + str(date.year) + ', ' + calendar.month_abbr[date.month] + ' ' + str(date.day) + ')'
     except: 
-        date = ''
+        if parsed:
+            date = string
+            date =  '(' + str(date.year) + ', ' + calendar.month_abbr[date.month] + ' ' + str(date.day) + ')'
+        else:
+            date = ''
     return date
 
 def Biblo(webpage,author,date):
+    
     if not 'http' in webpage:
         webpage = "http://" + webpage
     r  = requests.get(webpage)
@@ -39,6 +45,14 @@ def Biblo(webpage,author,date):
     title = soup.find_all('title')[0].get_text().strip()
     if title[len(title)-1] != r'.':
         title = title + r'.'
+    
+    article = Article(webpage)
+    article.download()
+    article.parse()
+    
+    
+    
+    
     midn = ''
     try:
         if not r'&' in author:
@@ -66,6 +80,53 @@ def Biblo(webpage,author,date):
     return(comb + ' ' + getdate(date) + '. ' + title + ' Retrieved from ' + webpage )
 
         
+   
+        
+def getauthor(self,author):
+    midn = ''
+    try:
+        if not r'&' in author:
+            lastn = author.split()[len(author.split())-1]
+            firstn = author.split()[0][0] + '.'
+            if len(author.split()) >2 :
+                midn = author.split()[1][0] + '.'
+            comb = lastn + ',' + firstn + midn
+        else:
+            
+            comb = ''
+            for auth in author.split(' &'):
+               lastn = auth.split()[len(auth.split())-1]
+               firstn = auth.split()[0][0] + '.'
+               if len(auth.split()) >2 :
+                   midn = author.split()[1][0] + '.'
+               name = lastn + ', ' + firstn
+               if comb != '':
+                   comb = comb + ' & '
+               comb = comb + name
+    except:
+        comb = author
+    
+    return comb
+
+class reference:
+         
+    def __init__(self,webpage):
+        self.webpage = webpage
+        article = Article(webpage)
+        article.download()
+        article.parse()
+        self.title = article.title
+        self.author = getauthor(self,article.authors[0])
+        self.date = getdate(article.publish_date,True)
+        
+
+
+    def getsource(self):
+        source = self.author + ' ' + self.date + '. ' + self.title + ' Retrieved from ' + self.webpage
+        return(source)
+
+
+
         
 def main():
     file = 'Table.xlsx'
